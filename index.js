6e77e2ec25b3cdd88b842b6a60ada591ba506fc4
Kyle Graehl <kgraehl@gmail.com>
2014-03-12 12:06:43 -0700

    add node-exif and start modifying it

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+window.exports = {}
+function reload() { chrome.runtime.reload() }
+
 function handleLaunch(launchInfo) {
     console.log('handleLaunch',launchInfo)
+
+    var items = launchInfo.launchData.items
+    if (items) {
+        for (var i=0; i<items.length; i++) {
+            addEntry(items[i].entry)
+        }
+    }
+}
+
+function addEntry(entry) {
+    console.log('have entry',entry)
+    // analyze entry metadata...
+    new ExifImage({image:entry}, function(res) {
+        console.log('exif result',res)
+    })
+
 }
